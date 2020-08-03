8d8b777965fd211f15033822b9912917e23517e1
Sporanzi <arnaud.cavat@gmail.com>
2020-08-03 14:35:40 +0200

    Player stats responsiveness improvement

--- src/components/PlayerStats.vue
+++ src/components/PlayerStats.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="row">
-    <div class="col">
+    <div class="col-md-4 col-12">
       <div class="jumbotron jumbotron-small first text-primary">
         <h2>{{ total }} Matchs</h2>
         <p>Points: {{ elo }} </p>
       </div>
     </div>
-    <div class="col">
+    <div class="col-md-4 col-12">
       <div class="jumbotron jumbotron-small text-success">
         <h2 class="">{{ win }} Victoires</h2>
         <p>{{ ((win / total) * 100).toFixed(2) }} %</p>
       </div>
     </div>
-    <div class="col">
+    <div class="col-md-4 col-12">
       <div class="jumbotron jumbotron-small text-danger">
         <h2>{{ loose }} Défaites</h2>
         <p>{{ ((loose / total) * 100).toFixed(2) }} %</p>
