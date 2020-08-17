81d8661ffccf648a93f2f23730e7ba49e2021f1f
Sporanzi <arnaud.cavat@gmail.com>
2020-08-17 17:11:34 +0200

    Use popover if is mobile

--- src/components/MatchTable.vue
+++ src/components/MatchTable.vue
@@ -39,25 +39,36 @@
         <td :class="getMatchCssClass(2, match)">{{ match.player2_elo }}</td>
         <td :class="getMatchCssClass(2, match)">{{ match.player2_previous_elo }}</td>
         <td class="allignr" :class="getMatchCssClass(2, match)">{{ player2.name }}</td>
-        <td :class="getMatchCssClass(2, match)"
-          class="character-thumbnail allignr hidden-xs">
+        <td :class="getMatchCssClass(2, match)" class="character-thumbnail allignr hidden-xs">
           <character-thumbnail :characters="match.character2 || player2.main_character" />
         </td>
         <td class="borderl">
+          <v-popover v-if="match.completed_at && isMobile">
+            <v-icon class="text-primary" name="check" />
+            <template slot="popover">
+              <a v-close-popover>Terminé {{ match.completed_at | formatPrettierAndWeek }}</a>
+            </template>
+          </v-popover>
           <v-icon
             class="text-primary"
             v-tooltip.right="
               `Terminé ${$options.filters.formatPrettierAndWeek(match.completed_at)}`
             "
-            v-if="match.completed_at"
+            v-if="match.completed_at && !isMobile"
             name="check"
           />
         </td>
         <td>
+          <v-popover v-if="match.moderated_at && isMobile">
+            <v-icon class="text-success" name="check-square" />
+            <template slot="popover">
+              <a v-close-popover>Modéré {{ match.moderated_at | formatPrettier }}</a>
+            </template>
+          </v-popover>
           <v-icon
             class="text-success"
             v-tooltip.right="`Modéré ${$options.filters.formatPrettier(match.moderated_at)}`"
-            v-if="match.moderated_at"
+            v-if="match.moderated_at && !isMobile"
             name="check-square"
           />
         </td>
@@ -67,7 +78,8 @@
             :open="isVideoOpen === match.id"
             offset="16"
             @auto-hide="videoClosed"
-            :auto-hide="true">
+            :auto-hide="true"
+          >
             <v-icon
               v-if="match.video && match.video.length"
               name="youtube"
@@ -75,15 +87,12 @@
               class="click text-danger"
             />
             <template slot="popover">
-                <p v-for="(video, index) in match.video" :key="index">
-                  <a
-                    :href="video.url"
-                    target="_blank"
-                    rel="noopener"
-                  >
-                    {{ video.title || video.url }}
-                  </a>
-                  </p>
+              <p v-for="(video, index) in match.video" :key="index">
+                <a
+                  :href="video.url"
+                  target="_blank" rel="noopener"
+                >{{ video.title || video.url }}</a>
+              </p>
             </template>
           </v-popover>
         </td>
@@ -119,6 +128,12 @@ export default {
     return {
       isVideoOpen: false,
     };
+  },
+
+  computed: {
+    isMobile() {
+      return window.matchMedia('(max-width: 760px)').matches;
+    },
   },
 
   methods: {
