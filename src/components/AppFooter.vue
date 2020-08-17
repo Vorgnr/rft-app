3a8e4c22116bf82bd63e83342fded64b382c59c2
Sporanzi <arnaud.cavat@gmail.com>
2020-08-17 16:40:52 +0200

    Better responsive footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,27 +2,27 @@
   <footer style="margin-top: 1.5rem" class="page-footer font-small pt-4 bg-dark">
     <div class="container text-center">
       <div class="row">
-        <div class="col">
+        <div class="col-md-2">
           <p>
             <router-link tag="a" to="/" class="center">Retour</router-link>
           </p>
         </div>
-        <div class="col">
+        <div class="col-md-2">
           <p>
             <a href="#top">Haut de la page</a>
           </p>
         </div>
-        <div class="col">
+        <div class="col-md-2">
           <p>
             <router-link tag="a" to="/rules" class="center">Règles du ranking</router-link>
           </p>
         </div>
-        <div class="col">
+        <div class="col-md-2">
           <p>
             <a target="_blank" href="https://discord.gg/7Ejf6b3" rel="noopener">Discord</a>
           </p>
         </div>
-        <div class="col">
+        <div class="col-md-3">
           <p>
             © 2020 Copyright
             <a href="http://ranking-tekken.fr/">
